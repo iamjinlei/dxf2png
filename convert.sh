9aaee5598623d82fa78e19ed343a76b39d1c69dc
Lei Jin <i.am.jin.lei@gmail.com>
2018-07-21 09:24:53 +0800

    update convert.sh

--- convert.sh
+++ convert.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-#docker run -i -t --rm -v `pwd`:/data -u=$UID:$(id -g $USER) dxf2png:latest bash -c "pwd; xvfb-run -a /dxf2png/dxf2png /data/$1 /data/$2"
-#docker run -i -t --rm -v `pwd`:/data -u=$UID:$(id -g $USER) dxf2png:latest bash -c "pwd; xvfb-run -a /dxf2png/dxf2png /data/$1 /data/$2 black 1024 768"
-docker run -i -t --rm -v `pwd`:/data --entrypoint /bin/bash dxf2png:latest
+if [[ $1 == "bash" ]]; then
+    docker run -i -t --rm -v `pwd`:/data --entrypoint /bin/bash dxf2png:latest
+else
+    # $1 dxf file path relative to current directory
+    # $2 output file path relative to current directory. File exetension controls file type to convert (.png, .bmp, .jpg, .svg are supported)
+    # $3 (optional) output file background color: black, white, transparent
+    # $4 (optional) output file max width
+    # $5 (optional) output file max height
+    docker run -i -t --rm -v `pwd`:/data -u=$UID:$(id -g $USER) dxf2png:latest bash -c "pwd; xvfb-run -a /dxf2png/dxf2png /data/$1 /data/$2 $3 $4 $5"
+fi
